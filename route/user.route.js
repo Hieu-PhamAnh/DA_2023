a8a09a6ada9325f14c441ae97260db6bc84838ea
Hieu-PhamAnh <hieupa.b19cn254@stu.ptit.edu.vn>
2023-12-03 22:54:59 +0700

    add some note

--- route/user.route.js
+++ route/user.route.js
@@ -10,36 +10,70 @@ userRouter.post(
   userMiddleware.checkRequire,
   userMiddleware.checkExist,
   userController.create,
-);
-userRouter.post('/friend-requests', userController.addFriend);
+); // tao tai khoan
+userRouter.post(
+  '/friend-requests',
+  // authMiddleware.checkRequired,
+  // authMiddleware.verifiyToken,
+  userController.addFriend,
+); // gui loi moi ket ban
 userRouter.get('/', validateMiddleware.checkQuerry, userController.getAllUser);
+// get all user
 userRouter.get(
   '/search',
   validateMiddleware.checkQuerry,
   userController.getByUsername,
-);
-userRouter.get('/friends', userController.getUserFriends);
-userRouter.get('/friend-requests', userController.getFriendRequests);
-userRouter.get('/posts', userController.getUserPosts);
-userRouter.delete('/friend-requests', userController.delteFriendRequest);
+); // search by username
+userRouter.get(
+  '/friends',
+  authMiddleware.checkRequired,
+  authMiddleware.verifiyToken,
+  userController.getUserFriends,
+); // get user friends
+userRouter.get(
+  '/friend-requests',
+  authMiddleware.checkRequired,
+  authMiddleware.verifiyToken,
+  userController.getFriendRequests,
+); // get user friend request
+userRouter.get(
+  '/posts',
+  authMiddleware.checkRequired,
+  authMiddleware.verifiyToken,
+  userController.getUserPosts,
+); // get user posts
+userRouter.delete(
+  '/friend-requests',
+  authMiddleware.checkRequired,
+  authMiddleware.verifiyToken,
+  userController.delteFriendRequest,
+); // xoa friend request (can verify id va friend id)
 userRouter.delete(
   '/friends/:id',
   validateMiddleware.checkValidId,
+  authMiddleware.checkRequired,
+  authMiddleware.verifiyToken,
   userController.deleteFriend,
-);
+); // xoa ban (can verify id va friend id trong params)
 userRouter.get(
   '/:id',
   validateMiddleware.checkValidId,
+  authMiddleware.checkRequired,
+  authMiddleware.verifiyToken,
   userController.getUserByID,
-);
+); // get user by id
 userRouter.patch(
   '/:id',
   validateMiddleware.checkValidId,
+  authMiddleware.checkRequired,
+  authMiddleware.verifiyToken,
   userController.update,
-);
+); // update profile
 userRouter.delete(
   '/:id',
   validateMiddleware.checkValidId,
+  authMiddleware.checkRequired,
+  authMiddleware.verifiyToken,
   userController.delete,
-);
+); // xoa user
 module.exports = userRouter;
